6b232f2781e86fd154ccca9327e66532a1d77aba
mateo <mateo@192.168.1.11>
2022-03-13 16:34:20 -0300

    Prueba Repo

---
+++ TP1/src/punto1/Concurso.java
@@ -0,0 +1,10 @@
+package punto1;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Concurso {
+	private ArrayList<Participante> ListaParticipantes;
+	private LocalDate fechaInicio;
+	private LocalDate fechaFin;
+}
---
+++ TP1/src/punto1/Participante.java
@@ -0,0 +1,16 @@
+package punto1;
+
+public class Participante {
+	private String nombre;
+	private String apellido;
+	private String dni;
+	private int puntaje;
+
+	public Participante(String nombre, String apellido, String dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		puntaje = 0;
+	}
+
+}
