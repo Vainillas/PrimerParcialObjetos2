29d8e561e33a3af8fbf7ee1f55db0fd9907bfa05
Vainillas <90745063+Vainillas@users.noreply.github.com>
2022-05-07 04:04:11 -0300

    Delete TP Layering directory

--- TP Layering/Ej1/ar/unrn/tp3/modelo/RepositorioDeParticipantes.java
+++
@@ -1,7 +0,0 @@
-package ar.unrn.tp3.modelo;
-
-import java.sql.SQLException;
-
-public interface RepositorioDeParticipantes {
-	public boolean nuevoParticipante(Participante p) throws SQLException;
-}
